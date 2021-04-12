d295d28dd9e2d2dd49b4becdb25078c0028a9b32
junjun1023 <jjjunpersonal@gmail.com>
2021-04-12 07:12:55 +0000

    Add online test

--- code/epoch.py
+++ code/epoch.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from tqdm import tqdm
 from torch.utils.data import DataLoader
+import numpy as np
 
 def train_epoch(predictor, optimizer, dataloader, device, sample=5):
 
@@ -119,3 +120,111 @@ def test_epoch(predictor, dataset, device):
         predict += forward
 
         return predict, truth
+
+
+
+def online_trading(predictor, dataset, device):
+        
+        import math
+        from np.random import choice
+
+        predictor.eval()
+
+        predict = []
+        truth = []
+        forward = []
+
+        slot = 0
+        profit = 0
+        slots = []
+
+        dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=2)
+
+        for index, data in tqdm(enumerate(dataloader)):
+
+                ref, gt = data # batch, 100, 4
+                gt = gt[:, :, 0].to(device, dtype=torch.float)
+
+                ref = torch.unsqueeze(ref, 1) # batch, channels, 100, 4
+                ref = torch.transpose(ref, 2, 3) # batch, channels, 4, 100
+
+                ref = ref.to(device, dtype=torch.float)
+
+                _, pr = predictor(ref)
+                
+                pos = torch.isnan(gt)
+                pos = ~ pos
+                
+                gt = gt[pos]
+                pr = pr[pos]
+
+                pr = pr.detach().cpu().numpy().tolist()
+                gt = gt.detach().cpu().numpy().tolist()
+
+                truth.append(gt[0])
+                
+                if len(forward) == 0:
+                        forward = pr
+                        forward = [p / math.pow(2, i) for i, p in enumerate(forward)]
+                        
+
+                else:
+                        predict.append(forward[0])
+                        forward = forward[1:] # pop the first element
+                        forward += [0]
+                        
+                        pr = [p / math.pow(2, i+1) for i, p in enumerate(pr)]
+                        
+                        forward = [sum(x) for x in zip(forward, pr)]
+
+
+                min_indices = np.argsort(np.array(forward))         # min to max
+                max_indices = min_indices[::-1]         # max to min
+                max_dist = [1/math.pow(2, i+1) for i in range(len(max_indices))]
+                min_dist = [1/math.pow(2, i+1) for i in range(len(min_indices))]
+
+                max_dist = [x for _, x in sorted(zip(max_indices, max_dist))]
+                min_dist = [x for _, x in sorted(zip(min_indices, min_dist))]
+
+                draw_max = choice(np.array(forward), 1, p=max_dist)
+                draw_min = choice(np.array(forward), 1, p=min_dist)
+
+                if slot == 0:
+                        if draw_min == 0:
+                                print("buy")
+                                slot = 1
+                                profit = profit - gt[0]
+                                slots.append(1)
+                        elif draw_max == 0:
+                                print("sell")
+                                slot = -1
+                                profit = profit + gt[0]
+                                slots.append(-1)
+                        else:
+                                print("no thing")
+                                slots.append(0)
+                elif slot == 1:
+                        if draw_max == 0:
+                                print("sell")
+                                slot = 0
+                                profit = profit + gt[0]
+                                slots.append(-1)
+                        else:
+                                print("no thing")
+                                slots.append(0)
+                else:
+                        if draw_min == 0:
+                                print("buy")
+                                slot = 1
+                                profit = profit - gt[0]
+                                slots.append(1)
+                        else:
+                                print("no thing")
+                                slots.append(0)
+
+
+
+
+        # predict += forward
+        # return predict, truth
+        return slots
